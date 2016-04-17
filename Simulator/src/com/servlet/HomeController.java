51c7fdcb64507117f3fde14011aad4127489e3d5
TomYanovich@gmail.com <TomYanovich@gmail.com>
2016-04-17 19:22:02 +0300

    ...

--- Simulator/src/com/servlet/HomeController.java
+++ Simulator/src/com/servlet/HomeController.java
@@ -28,7 +28,6 @@ public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private String courseName;
 
-
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */

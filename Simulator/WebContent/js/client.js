d7eb64c927d814c496748c1b436665704fe56bb5
Shanny9 <Shanny9@SHANNY>
2016-04-17 11:53:04 +0300

    client.js changes

--- Simulator/WebContent/js/client.js
+++ Simulator/WebContent/js/client.js
@@ -11,6 +11,9 @@ $(document).ready(function() {
     initialize page
     */
 	$.backstretch("./css/home_images/airplane-login.jpg"); //Fullscreen background
+    $('.login-form input[type="text"], .login-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
 	getSolutions(); // init solutionsData
 	$("#submit").click(sendSolution);
 	
@@ -21,29 +24,43 @@ function sendSolution (){
 	var sol = $('#solution').val();
 	var isCorrect = false;
 	
-	$.each(solutionsData, function(i, item) {
-		if(item.event == eventID && item.solution == sol){
-			isCorrect = true;
-			return false; // << break
-		}
-		
-	});//end for each
-	
-	if(isCorrect){
-/*		$.ajax({
-			url : "ClientController?action=sendSolution",
-			dataType : "json",
-			data: {eventID: eventID, solution: sol},
-			success : function(data) {
-				
-			},
-			error : function(e) {
-				console.log("js:sendSolution: Error in sendSolution.");
+	if(eventID != "" && sol!=""){
+		$.each(solutionsData, function(i, item) {
+			if(item.event == eventID && item.solution == sol){
+				isCorrect = true;
+				return false; // << break
 			}
-		});*/
-		console.log("Correct!");
-	}
+			
+		});//end for each
 		
+		if(isCorrect){
+	/*		$.ajax({
+				url : "ClientController?action=sendSolution",
+				dataType : "json",
+				data: {team: team, course: courseName, eventID: eventID, solution: sol},
+				success : function(data) {
+					
+				},
+				error : function(e) {
+					console.log("js:sendSolution: Error in sendSolution.");
+				}
+			});*/
+			console.log("Correct!");
+		}
+		else{	    	
+
+			return false;
+		}
+	}//end <if fields are empty>	
+	else{
+    	if( eventID == "" ) {
+    		$('#eventID').addClass('input-error');
+    	}
+    	if( sol == "" ) {
+    		$('#solution').addClass('input-error');
+    	}
+    	return false;
+	}
 }
 
 function getSolutions() {

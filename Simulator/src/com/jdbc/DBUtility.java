cff18bfe2afab3015b911c03ea0b8591e21e5746
Shanny9 <Shanny9@SHANNY.mynet>
2016-05-24 18:06:42 +0300

    some xml changes

--- Simulator/src/com/jdbc/DBUtility.java
+++ Simulator/src/com/jdbc/DBUtility.java
@@ -16,6 +16,7 @@ public class DBUtility {
 			/*String serverName = "132.75.252.108";
 			String portNumber = "3306";*/
 			String dbUrl = "jdbc:mysql://132.75.252.108:3306/SIMULATOR";
+//			String dbUrl = "jdbc:mysql://localhost:3306/SIMULATOR";
 			try {
 			Class.forName("com.mysql.jdbc.Driver");
 			// set the url, username and password for the database
